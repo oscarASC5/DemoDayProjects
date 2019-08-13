a77d3ef60c3b118108550bd492d9d481dfeac035
oscarASC5 <oscarandrade1210@gmail.com>
2019-08-13 11:22:59 -0400

    DemoDayProjects

---
+++ accountsettings.js
@@ -0,0 +1,18 @@
+// document.getElementById('a').onclick = function() {
+//     window.location.href = 'homepage.html';
+    
+//     return false;
+// }
+const btn = document.querySelector('#home');
+btn.addEventListener('click', function(){
+    window.location.href = 'homepage.html';
+    
+    return false; 
+})
+let usr;
+firebase.auth().onAuthStateChanged(function(user){
+    usr= user;
+    if (user){
+        console.log(user.uid);
+    }
+})
