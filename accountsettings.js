a06b4626769a3a5ef3a4091904d83081192e6119
oscarASC5 <oscarandrade1210@gmail.com>
2019-08-14 11:21:38 -0400

    DemoDayProjects

--- accountsettings.js
+++ accountsettings.js
@@ -16,3 +16,39 @@ firebase.auth().onAuthStateChanged(function(user){
         console.log(user.uid);
     }
 })
+
+
+//   firebase.auth().onAuthStateChanged(function(user){
+//     // if user is logged in
+// ​
+//   if (user){
+//       console.log(user.uid);
+// ​
+//       firebase.database().ref(user.uid).on('value', function(data){
+//       console.log(data.val());
+//       document.getElementById('name').value = data.val().Name
+//       document.getElementById('age').value = data.val().Age
+//       document.getElementById('location').value = data.val().Location
+//       document.getElementById('sport').value = data.val().Sport
+//       document.getElementById('level').value = data.val().SkillLevel
+      
+// ​
+//     })
+//   }
+// })
+// ​
+//   function writeData(event){
+//       console.log(event)
+//     event.preventDefault();
+//   if (usr){
+//     firebase.database().ref(usr.uid).set({
+//         Name: document.getElementById("name").value,
+//         Age: document.getElementById("age").value,
+//         Location: document.getElementById("location").value,
+//         Sport: document.getElementById("sport").value,
+//         SkillLevel: document.getElementById("level").value
+        
+        
+//     });
+    
+//       }}
