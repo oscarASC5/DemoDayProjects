415b7f8d73729039b511fd7942dd78459191652d
oscarASC5 <oscarandrade1210@gmail.com>
2019-08-15 11:10:45 -0400

    DemoDayProjects

--- accountsettings.js
+++ accountsettings.js
@@ -1,54 +1,17 @@
-// document.getElementById('a').onclick = function() {
-//     window.location.href = 'homepage.html';
-    
-//     return false;
-// }
-const btn = document.querySelector('#home');
-btn.addEventListener('click', function(){
-    window.location.href = 'homepage.html';
-    
-    return false; 
+const name = document.querySelector("#name")
+console.log(name.value)
+ //prescribed medication 
+const title = document.querySelector("#sport")
+console.log(title.value)
+const btn = document.querySelector("#Save")
+btn.addEventListener("click", function() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            firebase.database().ref(user.uid).set({
+                name: name.value,
+                meds: sport.value
+            });
+        }
+    })
+    console.log(name.value, sport.value)
 })
-let usr;
-firebase.auth().onAuthStateChanged(function(user){
-    usr= user;
-    if (user){
-        console.log(user.uid);
-    }
-})
-
-
-//   firebase.auth().onAuthStateChanged(function(user){
-//     // if user is logged in
-// ​
-//   if (user){
-//       console.log(user.uid);
-// ​
-//       firebase.database().ref(user.uid).on('value', function(data){
-//       console.log(data.val());
-//       document.getElementById('name').value = data.val().Name
-//       document.getElementById('age').value = data.val().Age
-//       document.getElementById('location').value = data.val().Location
-//       document.getElementById('sport').value = data.val().Sport
-//       document.getElementById('level').value = data.val().SkillLevel
-      
-// ​
-//     })
-//   }
-// })
-// ​
-//   function writeData(event){
-//       console.log(event)
-//     event.preventDefault();
-//   if (usr){
-//     firebase.database().ref(usr.uid).set({
-//         Name: document.getElementById("name").value,
-//         Age: document.getElementById("age").value,
-//         Location: document.getElementById("location").value,
-//         Sport: document.getElementById("sport").value,
-//         SkillLevel: document.getElementById("level").value
-        
-        
-//     });
-    
-//       }}
---
+++ save.js
@@ -0,0 +1,7 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        firebase.database().ref(user.uid).on('value', function(data) {
+            console.log(data.val());
+        })
+    }
+})
