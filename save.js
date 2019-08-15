08a78e703cd10d9680a8ef62fb78a65ed2a95cae
oscarASC5 <oscarandrade1210@gmail.com>
2019-08-15 13:46:11 -0400

    DemoDayProjects

--- save.js
+++ save.js
@@ -2,6 +2,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         firebase.database().ref(user.uid).on('value', function(data) {
             console.log(data.val());
+
+        
         })
     }
 })
